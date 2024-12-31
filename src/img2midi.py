64cf5b0dd836e74c24b0796d0aa78a024c7ee562
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-12-31 20:47:24 +0900

    a

--- src/img2midi.py
+++ src/img2midi.py
@@ -2,19 +2,20 @@ import numpy as np
 import mido
 from mido import Message, MidiFile
 from PIL import Image
+from typing import Literal
 
 
 def count_same(array):
     results = []
-    for row in array.copy():
+    for row in array:
         boundaries = np.flatnonzero(row[:-1] != row[1:]) + 1
 
         starts = np.concatenate([[0], boundaries])
         ends = np.concatenate([boundaries, [len(row)]])
-        lengths = ends - starts
+        lengths = (ends - starts).astype(np.int64)
         values = row[starts]
 
-        result = [(values[i], lengths[i]) for i in range(len(lengths))]
+        result = [[int(values[i]), int(lengths[i])] for i in range(len(lengths))]
         results.append(result)
     return results
 
@@ -25,18 +26,36 @@ def pil_to_midi(src: Image.Image, output_midi_file):
 
     midi = MidiFile()
     track = mido.MidiTrack()
-    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(120)))
     midi.tracks.append(track)
+    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(120)))
 
     print(len(img_array), len(img_array[0]))
-    offset = 60
-    for height_offset, i in enumerate(count_same(img_array)):
-        print(i)
-        for imdata in i:
-            pass
-            # track.append(
-            #     Message(
-            #         "note_on", note=64 + height_offset, velocity=100, time=offset + 0
-            #     )
-            # )
-    # WIP
+    counted = count_same(img_array)
+    offset = 120
+
+    info: list[tuple[Literal[0, 1], int, int]] = []
+    for height_offset, i in enumerate(counted):
+        now = 0
+        for im_data in i:
+            if im_data[0] == 0:
+                o = offset - height_offset
+                info.append((0, o, now))
+                info.append((1, o, now + im_data[1]))
+            now += im_data[1]
+    print(info)
+    info.sort(key=lambda x: x[2])
+    ooooo = 0
+    for opcode, note, t in info:
+        aa = t * 15 - ooooo
+        msg_type = "note_on" if opcode == 0 else "note_off"
+        track.append(Message(msg_type, note=note, velocity=100, time=aa))
+        ooooo += aa
+    midi.save(output_midi_file)
+
+
+if __name__ == "__main__":
+    a = Image.open("assets/badapple/a.jpg")
+    a = a.resize((round(a.width * 44 / a.height), 44))
+    # a = a.resize((127, round(a.height * 127 / a.width)))
+    a.save("assets/badapple/a2.jpg")
+    pil_to_midi(a, "assets/test.mid")
